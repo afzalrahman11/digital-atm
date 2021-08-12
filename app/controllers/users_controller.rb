9fe313b9c6509ce8482515780dd15092d3db40c5
Afzal Rahman <afzalrahman123@gmail.com>
2021-08-12 13:12:54 +0530

    Add user restrictions to view user profiles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,22 @@
 class UsersController < ApplicationController
+  before_action :set_user
+  before_action :require_same_user, only: [:show]
 
   def show
-    @user = User.find(params[:id])
+    
   end
+
+  private
+
+    def require_same_user
+      if @user != current_user
+        flash[:alert] = "You can not perform that action"
+        redirect_to current_user
+      end
+    end
+
+    def set_user
+      @user = User.find(params[:id])
+    end
 
 end
