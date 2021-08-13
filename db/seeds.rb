459411d994822aaf76eec0d38ff9f45a704e82d4
Afzal Rahman <afzalrahman123@gmail.com>
2021-08-13 18:13:01 +0530

    Seed added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,52 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "========== Starting Seed =========="
+
+
+["Afzal Rahman", "Muhammed U K", "Arjun K R", "Jabir Ali"].each_with_index do |name, index|
+  converted_name = name.gsub(' ','_').downcase
+  user = User.create(
+    name: name, 
+    email: "#{converted_name}@gmail.com",
+    password: converted_name
+  )
+
+  account = Account.create(
+    account_no: "112200#{index}",
+    account_type: "Savings",
+    balance: 25000,
+    user_id: user.id
+  )
+
+  Transaction.create(
+    account_id: account.id,
+    amount: rand(5000..10000),
+    tr_action: "Debit"
+  )
+end
+
+user = User.first
+Account.create(
+    account_no: "2222000",
+    account_type: "Current",
+    balance: 10000,
+    user_id: user.id
+  )
+
+user.accounts.each do |account|
+  5.times do
+    Transaction.create(
+      account_id: account.id,
+      amount: rand(1000..2000),
+      tr_action: "Credit"
+    )
+  end
+
+  5.times do
+    Transaction.create(
+      account_id: account.id,
+      amount: rand(500..800),
+      tr_action: "Debit"
+    )
+  end
+end
+
+puts "***** Seeding Completed *****"
